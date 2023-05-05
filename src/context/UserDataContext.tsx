445b156f74dc493b774ba152e2f2c5e0b0421a8a
Joseph Goolic <joegoolic15@gmail.com>
2023-05-05 11:52:13 -0400

    Chore:Corrected descriptions for skills/projects"

--- src/context/UserDataContext.tsx
+++ src/context/UserDataContext.tsx
@@ -15,7 +15,7 @@ export function UserDataProvider({ children }) {
 
     useEffect(() => {
         function handleResize() {
-            if (window.innerWidth < 1024) {
+            if (window.innerWidth < breakpoints.sm) {
                 setThin(true)
             } else {
                 setThin(false)
